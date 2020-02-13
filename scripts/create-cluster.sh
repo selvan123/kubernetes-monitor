a51e580a3c8e5cca12c7b8254f81c5744fe67272
ivanstanev <istanev93@gmail.com>
2020-02-13 15:26:12 +0000

    f

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,27 +1,11 @@
 #! /bin/bash
 set -e
 
-CRC_VERSION=1.5.0
 OC_VERSION=v3.11.0
 OC_COMMIT=0cbc58b
 
-sudo apt update
-sudo apt install -y qemu qemu-kvm network-manager
-
-curl \
-  https://mirror.openshift.com/pub/openshift-v4/clients/crc/${CRC_VERSION}/crc-linux-amd64.tar.xz \
-  -o crc-linux-amd64.tar.xz
-
-tar xf crc-linux-amd64.tar.xz
-cd crc-linux-${CRC_VERSION}-amd64
-
-./crc setup
-./crc start 2>&1 > ../output.txt
-eval $(crc oc-env)
-cd ..
-
-# -------------------------
-LOGIN=$(grep -io "oc login -u kubeadmin -p [A-Za-z0-9\-]* https://[a-z0-9\:\.]*" output.txt)
+sudo echo '34.66.203.10 api.crc.testing oauth-openshift.apps-crc.testing' >> /etc/hosts
+echo "done !"
 
 curl \
   -L \
